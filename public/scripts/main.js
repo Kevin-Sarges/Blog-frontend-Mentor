ec5cbaa21167959ab3cb97222b3c71938eba18f9
Kevin Sarges <kevinsarges08@gmail.com>
2021-05-04 11:43:05 -0300

    Finalizando a header

---
+++ public/scripts/main.js
@@ -0,0 +1,20 @@
+const optionProduct = {
+  open() {
+    document.getElementById('listProduct').classList.toggle('active');
+    document.getElementById('iconProduct').classList.toggle('image');
+  }
+}
+
+const optionCompany = {
+  open() {
+    document.getElementById('listCompany').classList.toggle('active');
+    document.getElementById('iconCompany').classList.toggle('image');
+  }
+}
+
+const optionConnect = {
+  open() {
+    document.getElementById('listConnect').classList.toggle('active');
+    document.getElementById('iconConnect').classList.toggle('image');
+  }
+}
